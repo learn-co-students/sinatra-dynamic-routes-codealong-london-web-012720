87d75f80094fe6cc448019fd436c86a9b4028c8f
musta7188 <musta7188@hotmail.it>
2020-02-18 15:44:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,25 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
+  #typet in the terminal this with a random name
+#http://localhost:9393/goodbye/Mustafa
+  get '/goodbye/:name' do
+    @user_name = params[:name] 
+    "Goodbye, #{@user_name}."
+  end
+
+  get '/multiply/:num1/:num2' do 
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @result = @num1 * @num2
+    "result #{@result}"
+
+  end
+
+  #Goodbye  Mustafa(this is the output I got )
 
   # Code your final two routes here:
 
 end
+
+
